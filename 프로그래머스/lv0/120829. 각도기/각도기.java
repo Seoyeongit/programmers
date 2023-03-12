564e5b568b20bd97abbec05e8269512e85be7bc7
SEOYEON LEE <110285874+Seoyeongit@users.noreply.github.com>
2023-03-12 19:04:02 +0900

    [level 0] Title: 각도기, Time: 0.03 ms, Memory: 77.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int angle) {
+        int answer = 0;
+        if (angle>0 && angle<=180) {
+            for (int i=0; i<=180; i++) {
+                if(angle < 90) {
+                    return 1;
+                } else if (angle == 90) {
+                    return 2;
+                } else if (angle > 90 && angle < 180) {
+                    return 3;
+                } else if (angle == 180) {
+                    return 4;
+                }
+            }
+        }
+        return answer;
+    }
+}
